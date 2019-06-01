907cf85150fef202fb766e57f893aeb97c616eac
Chaz Rowe <chazr_88@hotmail.com>
2019-06-01 00:00:55 -0400

    ran out of time on the styled components becasue when I imported the npm it broke all of my proptypes. After some research it appears this is a common issue

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,16 +1,60 @@
 import React from 'react';
+import styled from "styled-components";
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+    <HeaderStyle className="">
+      <HeaderStyleSpan><HeaderStyleSpanDate className="">SMARCH 32, 2018</HeaderStyleSpanDate></HeaderStyleSpan>
+      <HeaderStyleH1>Lambda Times</HeaderStyleH1>
+      <HeaderStyleSpan><HeaderStyleSpanTemp className="">98°</HeaderStyleSpanTemp> </HeaderStyleSpan>
+    </HeaderStyle>
   )
 }
+
+const HeaderStyle = styled.div `
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+`;
+
+const HeaderStyleH1 = styled.div `
+  font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: bold;
+  flex: 8;
+  text-align: center;
+  color: #000;
+`;
+
+const HeaderStyleSpan = styled.div `
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+`;
+
+const HeaderStyleSpanDate = styled.div `
+  margin-left: 25px;
+  flex: 1;
+`;
+
+const HeaderStyleSpanTemp = styled.div `
+.header .temp {
+  text-align: right;
+  margin-right: 25px;
+  flex: 1;
+`;
+
 
 export default Header
